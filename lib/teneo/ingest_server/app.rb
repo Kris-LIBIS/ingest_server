7bb8a03858ea28b3b9522d68623cecf217e56a23
Kris Dekeyser <kris.dekeyser@libis.be>
2020-01-21 11:57:15 +0100

    API server: - getting rid of statefull selection - take user role into account - improved error messages

--- lib/teneo/ingest_server/app.rb
+++ lib/teneo/ingest_server/app.rb
@@ -10,6 +10,8 @@ module Teneo
 
     class App < Roda
 
+      ROLE = 'ingester'.freeze
+
       key = File.read(File.join(Teneo::IngestServer::ROOT_DIR, 'key.bin'), mode: 'rb')
 
       use Rack::Session::Cookie, secret: key
@@ -23,7 +25,7 @@ module Teneo
       plugin :halt
       plugin :request_headers
       plugin :sessions,
-             cookie_options: {http_only: true, same_site: :strict},
+             cookie_options: { http_only: true, same_site: :strict },
              secret: key,
              key: 'teneo.ingester'
 
@@ -31,19 +33,19 @@ module Teneo
 
         r.on 'api' do
 
-          r.halt 406 unless r.headers['Accept'] =~ %r{^application/json}
-          r.halt 405 unless r.headers['Content-Type'] =~ %r{^application/json}
+          r.halt 406, 'Unsupported Accept header value' unless r.headers['Accept'] =~ %r{^application/json}
+          r.halt 405, 'Unsupported Content-Type header value' unless r.headers['Content-Type'] =~ %r{^application/json}
 
           r.post 'login' do
             account = Account.authenticate(r.params['email'], r.params['password'])
             user = account&.user
-            r.halt(401) unless user
+            r.halt 401 unless user
             session.clear
             session[:user_id] = user.uuid
             {
                 name: user.name,
                 orgs: user.member_organizations.each_with_object({}) do |(org, roles), hash|
-                  hash[org.name] = {id: org.id, roles: roles}
+                  hash[org.name] = { id: org.id, roles: roles }
                 end
             }
           end
@@ -61,22 +63,20 @@ module Teneo
             }
           end
 
-          r.halt(401) unless current_user
+          r.halt 401 unless current_user
 
           r.on 'user' do
 
             r.is do
-              {first_name: current_user.first_name, last_name: current_user.last_name, email: current_user.email}
+              { first_name: current_user.first_name, last_name: current_user.last_name, email: current_user.email }
             end
 
           end
 
           r.on 'organizations' do
 
-            user_orgs = current_user.member_organizations
-
             r.is do
-              user_orgs.each_with_object([]) do |(org, roles), arr|
+              current_user.organizations_for(ROLE).each_with_object([]) do |(org, roles), arr|
                 arr << {
                     id: org.id,
                     name: org.name,
@@ -86,8 +86,22 @@ module Teneo
             end
 
             r.on Integer do |id|
-              org = user_orgs.keys.find { |o| o.id == id }
+
+              org = Teneo::DataModel::Organization.find_by(id: id)
               r.halt(404) unless org
+              r.halt(401) unless current_user.is_authorized?(ROLE, org)
+
+              r.is 'agreements' do
+                agreements = org.ingest_agreements
+
+                agreements.each_with_object([]) do |agr, arr|
+                  arr << {
+                      id: agr.id,
+                      name: agr.name,
+                      description: agr.description,
+                  }
+                end
+              end
 
               r.get do
                 {
@@ -97,35 +111,20 @@ module Teneo
                 }
               end
 
-              r.post do
-                case r.params['action']
-                when 'select'
-                  session[:org_id] = org.id
-                  {
-                      id: org.id,
-                      name: org.name,
-                  }
-                else
-                  r.halt(400)
-                end
-              end
             end
 
           end
 
-          # @return [Teneo::DataModel::Organization]
-          def current_organization
-            @current_organization ||= Teneo::DataModel::Organization.find_by(id: session[:org_id])
-          end
-
-          r.halt 412 unless current_organization
-
-          r.on 'ingest_agreements' do
-
-            org_agreements = current_organization&.ingest_agreements
+          r.on 'agreements' do
 
             r.is do
-              org_agreements.each_with_object([]) do |agr, arr|
+              org_id = r.params['org_id']
+              r.halt 412, [{ error: 'Missing org_id' }] unless org_id
+              organization = Teneo::DataModel::Organization.find_by(id: org_id)
+              r.halt 412, [{ error: 'Organization not found' }] unless organization
+              r.halt 401 unless current_user.is_authorized?(ROLE, organization)
+
+              organization.ingest_agreements.each_with_object([]) do |agr, arr|
                 arr << {
                     id: agr.id,
                     name: agr.name,
@@ -135,52 +134,56 @@ module Teneo
             end
 
             r.on Integer do |id|
-              agr = org_agreements.find_by(id: id)
-              r.halt(404) unless agr
+              agreement = Teneo::DataModel::IngestAgreement.find_by(id: id)
+              r.halt 404 unless agreement
+              r.halt 401 unless current_user.is_authorized?(ROLE, agreement)
 
-              r.get do
-                {
-                    name: agr.name,
-                    description: agr.description,
-                    project_name: agr.project_name,
-                    collection_name: agr.collection_name,
-                    collection_description: agr.collection_description,
-                    contact_ingest: agr.contact_ingest,
-                    contact_collection: agr.contact_collection,
-                    contact_system: agr.contact_system,
-                }
+              r.get 'workflows' do
+                agreement.ingest_workflows.map do |workflow|
+                  {
+                      id: workflow.id,
+                      name: workflow.name
+                  }
+                end
               end
 
-              r.post do
-                case (r.params['action'])
-                when 'select'
-                  session[:agr_id] = agr.id
-                  {
-                      id: agr.id,
-                      name: agr.name,
+              r.get 'packages' do
+                agreement.packages.each_with_object([]) do |package, arr|
+                  arr << {
+                      id: package.id,
+                      name: package.name,
+                      workflow: package.ingest_workflow.name
                   }
-                else
-                  r.halt(400)
                 end
+              end
+
+              r.is do
+                {
+                    name: agreement.name,
+                    description: agreement.description,
+                    project_name: agreement.project_name,
+                    collection_name: agreement.collection_name,
+                    collection_description: agreement.collection_description,
+                    contact_ingest: agreement.contact_ingest,
+                    contact_collection: agreement.contact_collection,
+                    contact_system: agreement.contact_system,
+                }
               end
 
             end
 
           end
 
-          # @return [Teneo::DataModel::Organization]
-          def current_agreement
-            @current_agreement ||= Teneo::DataModel::IngestAgreement.find_by(id: session[:agr_id])
-          end
-
-          r.halt 412 unless current_agreement
-
-          r.on 'ingest_workflows' do
-
-            workflows = current_agreement.ingest_workflows
+          r.on 'workflows' do
 
             r.is do
-              workflows.map do |workflow|
+              agr_id = r.params['agr_id']
+              r.halt 412, [{ error: 'Missing agr_id' }] unless agr_id
+              agreement = Teneo::DataModel::IngestAgreement.find_by(id: agr_id)
+              r.halt 412, [{ error: 'Agreement not found' }] unless agreement
+              r.halt 401 unless current_user.is_authorized?(ROLE, agreement)
+
+              agreement.ingest_workflows.map do |workflow|
                 {
                     id: workflow.id,
                     name: workflow.name
@@ -190,11 +193,22 @@ module Teneo
 
             r.on Integer do |id|
               workflow = Teneo::DataModel::IngestWorkflow.find_by(id: id)
-              r.halt(404) unless workflow
+              r.halt 404 unless workflow
+              r.halt 401 unless current_user.is_authorized?(ROLE, workflow)
 
-              r.get do
+              r.get 'packages' do
+                workflow.packages.map do |package|
+                  {
+                      id: package.id,
+                      name: package.name,
+                      workflow: package.ingest_workflow.name
+                  }
+                end
+              end
+
+              r.is do
                 {
-                    name: workflows.name,
+                    name: workflow.name,
                     description: workflow.description,
                     stages: workflow.ingest_stages.each_with_object({}) do |stage, hash|
                       hash[stage.name] = {
@@ -208,55 +222,61 @@ module Teneo
               end
 
             end
+
+
           end
 
           r.on 'packages' do
 
-            packages = current_agreement.packages
-
             r.is do
-              packages.each_with_object([]) do |package, arr|
-                arr << {
+
+              if (flow_id = r.params['flow_id'])
+                workflow = Teneo::DataModel::IngestWorkflow.find_by(id: flow_id)
+                r.halt 412, [{ error: 'Workflow not found' }] unless workflow
+                r.halt 401 unless current_user.is_authorized?(ROLE, workflow)
+                workflow
+              elsif (agr_id = r.params['agr_id'])
+                agreement = Teneo::DataModel::IngestAgreement.find_by(id: agr_id)
+                r.halt 412, [{ error: 'Agreement not found' }] unless agreement
+                r.halt 401 unless current_user.is_authorized?(ROLE, agreement)
+                agreement
+              else
+                r.halt 412, [{ error: 'Missing agr_id or flow_id' }]
+              end.packages.map do |package|
+                {
                     id: package.id,
                     name: package.name,
-                    workflow: package.ingest_workflow.name
+                    workflow: package.ingest_workflow.name,
                 }
               end
             end
 
             r.on Integer do |id|
-              package = packages.find_by(id: id)
-              r.halt(404) unless package
+              package = Teneo::DataModel::Package.find_by(id: id)
+              r.halt 404 unless package
+              r.halt 401 unless current_user.is_authorized?(ROLE, package)
 
-              r.get do
-                {
-                    id: package.id,
-                    name: package.name,
-                    workflow: {
-                        id: package.ingest_workflow.id,
-                        name: package.ingest_workflow.name
-                    },
-                    options: package.options,
-                }
+              r.get 'runs' do
+                package.runs.map do |run|
+                  {
+                      id: run.id,
+                      name: run.name,
+                      status: run.last_status.to_s
+                  }
+                end
               end
 
               r.post do
                 case r.params['action']
-                when 'select'
-                  session[:package_id] = package.id
-                  {
-                      id: package.id,
-                      name: package.name,
-                  }
                 when 'start'
-                  queue = Teneo::IngestServer::Queue.find_by(id: r.params['queue_id'])
-                  queue ||= Teneo::IngestServer::Queue.find_by(name: r.params['queue_name'])
+                  queue = Teneo::Ingester::Queue.find_by(id: r.params['queue_id'])
+                  queue ||= Teneo::Ingester::Queue.find_by(name: r.params['queue_name'])
                   priority = r.params['priority'] || 100
                   r.halt(412) unless queue
                   run = package.make_run
-                  work = Teneo::IngestServer::Work.create(
+                  work = Teneo::Ingester::Work.create(
                       queue: queue, priority: priority, subject: run, action: 'start',
-                      work_status: Teneo::IngestServer::WorkStatus.find_by(name: 'new')
+                      work_status: Teneo::Ingester::WorkStatus.find_by(name: 'new')
                   )
                   {
                       work_id: work.id,
@@ -268,24 +288,35 @@ module Teneo
                   r.halt(400)
                 end
               end
+
+              r.is do
+                {
+                    id: package.id,
+                    name: package.name,
+                    workflow: {
+                        id: package.ingest_workflow.id,
+                        name: package.ingest_workflow.name
+                    },
+                    options: package.options,
+                    parameters: package.parameter_values.transform_keys { |key| key.gsub(/^.*#/, '') }
+                }
+              end
+
             end
 
           end
 
-          # @return [Teneo::DataModel::Organization]
-          def current_package
-            @current_package ||= Teneo::DataModel::Package.find_by(id: session[:package_id])
-          end
-
-          r.halt 412 unless current_package
-
           r.on 'runs' do
 
-            runs = current_package.runs
-
             r.is do
-              runs.each_with_object([]) do |run, arr|
-                arr << {
+              pkg_id = r.params['pkg_id']
+              r.halt 412, [{ error: 'Missing pkg_id' }] unless pkg_id
+              package = Teneo::DataModel::Package.find_by(id: pkg_id)
+              r.halt 412, [{ error: 'Agreement not found' }] unless package
+              r.halt 401 unless current_user.is_authorized?(ROLE, package)
+
+              package.runs.map do |run|
+                {
                     id: run.id,
                     name: run.name,
                     status: run.last_status.to_s
@@ -294,18 +325,19 @@ module Teneo
             end
 
             r.on Integer do |id|
-              run = run.find_by(id: id)
-              r.halt(404) unless run
+              run = Teneo::DataModel::Run.find_by(id: id)
+              r.halt 404 unless run
+              r.halt 401 unless current_user.is_authorized?(ROLE, run)
 
-              r.get do
+              r.is do
                 {
                     id: run.id,
                     name: run.name,
-                    start: run.start_date.strftime('%Y/%m/%d %H:%M:%S.%L'),
+                    start: run.start_date&.strftime('%Y/%m/%d %H:%M:%S.%L'),
                     config: run.config,
                     options: run.options,
                     properties: run.properties,
-                    status: run.last_status(run).to_s
+                    status: run.last_status.to_s
                 }
               end
 
